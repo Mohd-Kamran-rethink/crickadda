e298df02673f65b3c062b0a520bdab9e8e718be5
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-06-17 16:43:13 +0530

    sdfsd

--- resources/views/MainTheme/index.blade.php
+++ resources/views/MainTheme/index.blade.php
@@ -1119,7 +1119,7 @@
                     </a>
                 </div>
                 <div class="fixed-footer-buttons user-signin-links ng-star-inserted"
-                    style="display: flex  !important;justify-content: center !important;margin-bottom: 20px">
+                    style="display: flex  !important;justify-content: center !important;width: 99%;margin-bottom: 20px">
                     <span class="signup_btn_wrapper" style="width: 36%">
                         <a href="https://app.cricadda.com/login"
                             class="signup waves-effect waves-light ng-star-inserted">
